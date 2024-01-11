eca472860681b9c105978b06b938e74188315618
Þór Vilhelm Jónatansson <thor@metadata.is>
2024-01-11 21:20:21 +0000

    Initial Commit

---
+++ AskTheApp/AskTheAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AskTheAppApp.swift
+//  AskTheApp
+//
+//  Created by Thor Jonatansson on 11.1.2024.
+//
+
+import SwiftUI
+
+@main
+struct AskTheAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AskTheApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AskTheApp
+//
+//  Created by Thor Jonatansson on 11.1.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
